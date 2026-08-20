6b42d00b5b657c51483504808c86ebc2317d3817
dipenkalal <171080107009.acet@gmail.com>
2026-08-20 20:47:43 +0000

    phase17: add isolated Greenhouse job connector

--- platform/backend/career/connectors/__init__.py
+++ platform/backend/career/connectors/__init__.py
@@ -6,12 +6,24 @@ from career.connectors.contracts import (
     CareerDiscoveryCandidate,
     CareerSourceConnectorKind,
 )
+from career.connectors.greenhouse import (
+    GREENHOUSE_BOARD_API_HOST,
+    GREENHOUSE_CONNECTOR_ID,
+    GREENHOUSE_MAX_JOBS,
+    GreenhouseConnectorParseError,
+    GreenhouseJobBoardConnector,
+)
 
 __all__ = [
+    "GREENHOUSE_BOARD_API_HOST",
+    "GREENHOUSE_CONNECTOR_ID",
+    "GREENHOUSE_MAX_JOBS",
     "CareerConnector",
     "CareerConnectorDescriptor",
     "CareerConnectorParseInput",
     "CareerConnectorResult",
     "CareerDiscoveryCandidate",
     "CareerSourceConnectorKind",
+    "GreenhouseConnectorParseError",
+    "GreenhouseJobBoardConnector",
 ]
